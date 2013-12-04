fa14baf3bb470f6d202f5728d8f698e2bd2c0aec
oranita <or.anita@gmail.com>
2013-12-04 11:51:43 -0500

    just gets title of one specific item from a specific url

--- CS4240Project/spiders/MacysSpider.py
+++ CS4240Project/spiders/MacysSpider.py
@@ -1,10 +1,10 @@
 from scrapy.spider import BaseSpider
 from scrapy.selector import Selector
 
-from CS4240Project.items import eBayItem
+from CS4240Project.items import MacysItem
 
-class eBaySpider(BaseSpider):
-   name = "ebay"
+class MacysSpider(BaseSpider):
+   name = "macys"
    allowed_domains = ["macys.com"]
    start_urls = [
        "http://www1.macys.com/shop/product/hotel-collection-modern-trellis-bedding-collection?ID=1121718&CategoryID=7502"
@@ -13,6 +13,6 @@ class eBaySpider(BaseSpider):
    def parse(self, response):
        sel = Selector(response)
        site = sel.xpath('//ul/li')
-       item = eBayItem()
+       item = MacysItem()
        item['title'] = site.xpath('//h1/text()').extract()
        return item
