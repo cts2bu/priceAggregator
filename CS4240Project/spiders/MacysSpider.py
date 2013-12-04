98e39cec2f6a97265cbbb7a78e90bcd03456a812
oranita <or.anita@gmail.com>
2013-12-04 18:53:46 -0500

    macys spider now gets all the item names from a particular bedding collection

--- CS4240Project/spiders/MacysSpider.py
+++ CS4240Project/spiders/MacysSpider.py
@@ -7,12 +7,15 @@ class MacysSpider(BaseSpider):
    name = "macys"
    allowed_domains = ["macys.com"]
    start_urls = [
-       "http://www1.macys.com/shop/product/hotel-collection-modern-trellis-bedding-collection?ID=1121718&CategoryID=7502"
+       "http://www1.macys.com/shop/bed-bath/bedding-collections?id=7502"
    ]
 
    def parse(self, response):
        sel = Selector(response)
-       site = sel.xpath('//ul/li')
-       item = MacysItem()
-       item['title'] = site.xpath('//h1/text()').extract()
-       return item
+       sites = sel.xpath('//div/div[@class="shortDescription"]')
+       items = []
+       for site in sites:
+           item = MacysItem()
+           item['title'] = site.xpath('a/text()').extract()
+           items.append(item)
+       return items
