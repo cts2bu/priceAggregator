bb570b0b4d4163978867fb3123caad033779b1f6
cts2bu <lot59ym>
2013-12-06 15:32:28 -0500

    Quick item change

--- priceAggregator/parsers/CSVParser.py
+++ priceAggregator/parsers/CSVParser.py
@@ -2,7 +2,7 @@ __author__ = 'Christopher'
 
 class CSVParser():
     def __init__(self):
-        print "This won't ever be seen"
+        self = self
 
     def printCSV(self):
         print "This won't ever be called"
