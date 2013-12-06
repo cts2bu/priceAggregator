7ec3c25f1338bc6faa194a84df27495236029c9e
oranita <or.anita@gmail.com>
2013-12-06 12:25:04 -0500

    put the start urls from ebay spider into a separate file, added another url

--- CS4240Project/starturls.py
+++ CS4240Project/starturls.py
@@ -1,4 +1,5 @@
 class StartUrls():
     ebayurls = [
-        "http://www.ebay.com/sch/i.html?_trksid=p2050601.m570.l1313.TR0.TRC0.Xheritage+door+handle+no+key+pad&_nkw=heritage+door+handle+no+key+pad&_sacat=0&_from=R40"
+        "http://www.ebay.com/sch/i.html?_trksid=p2050601.m570.l1313.TR0.TRC0.whole+grain+power&_nkw=whole+grain+power&_sacat=0&_from=R40",
+        "http://www.ebay.com/sch/i.html?_trksid=p2050601.m570.l1313.TR0.TRC0.gouda+statue&_nkw=gouda+statue&_sacat=0&_from=R40"
     ]
