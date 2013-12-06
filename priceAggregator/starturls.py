5de257aea868d74bc771b87572f8ae91aaac872a
oranita <or.anita@gmail.com>
2013-12-06 13:00:24 -0500

    builds url according to search terms for ebay, made so search terms are separated by spaces

--- priceAggregator/starturls.py
+++ priceAggregator/starturls.py
@@ -1,6 +1,7 @@
 class StartUrls():
-    searchterms = ["thai+diet", "slug+soda+rare"]
+    searches = ["thai diet", "slug soda rare"]
     ebayurls = []
-    for term in searchterms:
-        url = "http://www.ebay.com/sch/i.html?_trksid=p2050601.m570.l1313.TR0.TRC0."+term+"&_nkw="+term+"&_sacat=0&_from=R40"
+    for search in searches:
+        search.replace(" ","+")
+        url = "http://www.ebay.com/sch/i.html?_trksid=p2050601.m570.l1313.TR0.TRC0."+search+"&_nkw="+search+"&_sacat=0&_from=R40"
         ebayurls.append(url)
