7574135f528cd406f8981f72347800065a102c91
oranita <or.anita@gmail.com>
2013-12-05 21:16:10 -0500

    amazon spider gets prices

--- CS4240Project/spiders/AmazonSpider.py
+++ CS4240Project/spiders/AmazonSpider.py
@@ -10,7 +10,7 @@ class AmazonSpider(CrawlSpider):
    name = "amzn"
    allowed_domains = ["amazon.com"]
    start_urls = [
-       "http://www.amazon.com/s/ref=nb_sb_noss_1?url=search-alias%3Daps&field-keywords=bath%20bedding&sprefix=bath+bedd%2Caps&rh=i%3Aaps%2Ck%3Abath%20bedding"
+       "http://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=bath+bedding&rh=i%3Aaps%2Ck%3Abath+bedding"
    ]
 
    rules = (
@@ -20,18 +20,14 @@ class AmazonSpider(CrawlSpider):
 
    def parse_items(self, response):
        sel = Selector(response)
-       #sites = sel.xpath('//div[@class="btfResults"]')
+       sites = sel.xpath('//ul/li[@class="newp"]/a')
        items = []
-       #for site in sites:
-       item = AmazonItem()
-           #item['title'] = site.xpath('div[@class="shortDescription"]/a/text()').extract()
-       item['title'] = sel.xpath('//*[@class="result_0"]/h3/a/span/text()').extract()
-           #item['link'] = site.xpath('a/@href').extract()
-           #item['saleprice'] = site.xpath('div[@class="prices"]/span[@class="priceSale"]/text()').extract()
-           #item['origprice'] = site.xpath('div[@class="prices"]/span[not(contains(@class, "priceSale"))]/text()').extract()
-       items.append(item)
+       for site in sites:
+           item = AmazonItem()
+           item['title'] = site.xpath('span/text()').extract()
+           items.append(item)
        return items
 
 if __name__ == "__main__":
-    os.system('scrapy crawl amzn -o amazonscrape.json -t json')
+    os.system('scrapy crawl amzn -o amazonscrape5.json -t json')
 
