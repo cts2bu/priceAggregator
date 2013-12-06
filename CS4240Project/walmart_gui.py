7b45970ca186a7d9f724220ced98c92de67ab874
Piam Moradi <pm7dh@virginia.edu>
2013-12-06 11:38:05 -0500

    Minor changes to gui files.

--- CS4240Project/walmart_gui.py
+++ CS4240Project/walmart_gui.py
@@ -8,7 +8,7 @@ con.text_factory = str
 c.execute("create table if not exists walmart (col1, col2, col3, col4)")
 
 def insert_into_table():
-    csvfile = open('spiders/walmart.csv', 'rb')
+    csvfile = open('walmart.csv', 'rb')
     creader = csv.reader(csvfile, delimiter=',')
     headers = creader.next()
     for t in creader:
@@ -17,7 +17,7 @@ def insert_into_table():
 def display_GUI(col1_text, col2_text, col3_text, col4_text):
     root = Tk()
     root.wm_title("Table")
-    l = Label(root, text = "title, link, price, price2")
+    l = Label(root, text = "price2, price, link, title")
     l.pack()
     i = 0
     while i < len(col1_text):
@@ -27,12 +27,12 @@ def display_GUI(col1_text, col2_text, col3_text, col4_text):
     root.mainloop()
 
 insert_into_table()
-c.execute("select col1 from t")
+c.execute("select col1 from walmart")
 col1 = c.fetchall()
-c.execute("select col2 from t")
+c.execute("select col2 from walmart")
 col2 = c.fetchall()
-c.execute("select col3 from t")
+c.execute("select col3 from walmart")
 col3 = c.fetchall()
-c.execute("select col4 from t")
+c.execute("select col4 from walmart")
 col4 = c.fetchall()
 display_GUI(col1, col2, col3, col4)
