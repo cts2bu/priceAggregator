018616cfff7d6e992d169fc67c80f412653c6cdf
cts2bu <lot59ym>
2013-12-06 15:53:33 -0500

    Walmart CSV Parser all good to go.

--- priceAggregator/parsers/WalmartCSVParser.py
+++ priceAggregator/parsers/WalmartCSVParser.py
@@ -11,9 +11,9 @@ class WalmartCSVParser(CSVParser):
         reader.next() #skip the first title line
         for row in reader:
             mainPrice = ''
-            mainPrice = row[0]
+            mainPrice = row[0][:len(row[0]) - 1]
             if mainPrice == '':
-                mainPrice = row[1]
+                mainPrice = row[1][:len(row[1]) - 1]
             link = row[2]
             title = row[3]
             print title + ' ' + mainPrice + ' ' + link
