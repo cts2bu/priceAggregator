da2afc01efed5ffd34abedc7cf93113be66491c6
cts2bu <lot59ym>
2013-12-07 00:26:30 -0500

    Fixing package calls.

--- priceAggregator/main/main.py
+++ priceAggregator/main/main.py
@@ -1,5 +1,5 @@
 __author__ = 'piammoradi'
-from priceAggregator.starturls import StartUrls
+from priceAggregator.spiders.starturls import StartUrls
 
 input_search = raw_input("Search for items: ")
 urls = StartUrls(input_search)
