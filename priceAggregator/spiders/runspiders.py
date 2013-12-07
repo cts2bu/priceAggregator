ec705e266faa9f005c40278fec25c25bdbf4c360
cts2bu <lot59ym>
2013-12-07 15:31:07 -0500

    Build script-y files

--- priceAggregator/spiders/runspiders.py
+++ priceAggregator/spiders/runspiders.py
@@ -1,6 +1,12 @@
 import os
+from priceAggregator.spiders.starturls import StartUrls
 
 if __name__ == "__main__":
-    os.system('scrapy crawl amzn -o amazonscrape.csv -t csv')
-    os.system('scrapy crawl ebay -o ebayscrape.csv -t csv')
-    os.system('scrapy crawl walmart -o walmartscrape.csv -t csv')
+    input = raw_input("Please enter a search term. ")
+    url = StartUrls(input)
+    print "Running amazon spider..."
+    os.system('scrapy crawl amzn -a start_url="' + url.amazonurl + '" -o amazonscrape.csv -t csv --nolog')
+    print "Running ebay spider..."
+    os.system('scrapy crawl ebay -a start_url="' + url.ebayurl + '" -o ebayscrape.csv -t csv --nolog')
+    print "Running walmart spider..."
+    os.system('scrapy crawl walmart -a start_url="' + url.walmarturl + '" -o walmartscrape.csv -t csv --nolog')
