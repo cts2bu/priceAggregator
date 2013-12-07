73bb7b0d0ac124ebb03447f8e2a4d93823df6696
Piam Moradi <pm7dh@virginia.edu>
2013-12-07 13:03:28 -0500

    Main file that starts to run scrapy based on user input in GUI

--- priceAggregator/main/main.py
+++ priceAggregator/main/main.py
@@ -1,17 +1,34 @@
 __author__ = 'piammoradi'
 from priceAggregator.spiders.starturls import StartUrls
 import os
+from Tkinter import *
 #from priceAggregator.GUIs import run_guis
 
-input_search = raw_input("Search for items: ")
-urls = StartUrls(input_search)
+class Main():
+    def __init__(self):
+        self.urls = StartUrls("")
+    def display(self):
+        root = Tk()
+        root.wm_title("Search")
+        e = Entry(root)
+        e.pack()
+        e.focus_set()
+        b = Button(root, text = "Search", command =lambda: self.run_spiders())
+        b.pack()
+        root.mainloop()
+        self.urls = StartUrls(e.get())
+    def run_spiders(self):
+        print "Running amazon spider..."
+        os.system('scrapy crawl amzn -a start_url="' + self.urls.amazonurl + '" -o amazonscrape.csv -t csv --nolog')
+        print "Running ebay spider..."
+        os.system('scrapy crawl ebay -a start_url="' + self.urls.ebayurl + '" -o ebay.csv -t csv --nolog')
+        print "Running walmart spider..."
+        os.system('scrapy crawl walmart -a start_url="' + self.urls.walmarturl + '" -o walmart.csv -t csv --nolog')
+new_main = Main()
+new_main.display()
 
-print "Running amazon spider..."
-os.system('scrapy crawl amzn -a start_url="' + urls.amazonurl + '" -o amazonscrape.csv -t csv --nolog')
-print "Running ebay spider..."
-os.system('scrapy crawl ebay -a start_url="' + urls.ebayurl + '" -o ebay.csv -t csv --nolog')
-print "Running walmart spider..."
-os.system('scrapy crawl walmart -a start_url="' + urls.walmarturl + '" -o walmart.csv -t csv --nolog')
+
+
 
 #run_guis.run_GUI().show_menu()
 
