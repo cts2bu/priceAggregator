e03c85928ba345d99916784c00b74c5acdaff8df
cts2bu <lot59ym>
2013-12-07 15:42:05 -0500

    Changed around WalmartCSVParser (price field now done differently)

--- priceAggregator/parsers/WalmartCSVParser.py
+++ priceAggregator/parsers/WalmartCSVParser.py
@@ -6,6 +6,6 @@ import csv
 class WalmartCSVParser(CSVParser):
 
     def printCSV(self, row):
-        mainPrice = row[0][:len(row[0]) - 1]
+        mainPrice = row[1][:len(row[1]) - 1]
         title = row[3]
         return title + ' ' + mainPrice
